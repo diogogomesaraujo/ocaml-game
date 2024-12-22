50a09106fdee1092f6d1019ae88c45ede1d93641
diogogomesaraujo <118282799+diogogomesaraujo@users.noreply.github.com>
2024-12-22 04:22:10 +0000

    collisions

--- bin/main.ml
+++ bin/main.ml
@@ -1,34 +1,38 @@
 open Raylib
 
 type player = {
-  pos : Vector2.t;
-  size : Vector2.t;
+  body : Rectangle.t;
   color : Color.t;
 }
 
 let p = {
-  pos = Vector2.create 400.0 300.0;
-  size = Vector2.create 20.0 20.0;
+  body = Rectangle.create 200.0 300.0 20.0 20.0;
   color = Color.black;
 }
+
+let e = {
+  body = Rectangle.create 400.0 200.0 20.0 20.0;
+  color = Color.red;
+}
+
 let p_move p =
   let p_move_x =
     if is_key_down Key.A || is_key_down Key.Left then
-      Vector2.create ((Vector2.x p.pos) -. 5.0) (Vector2.y p.pos)
+      Rectangle.create ((Rectangle.x p.body) -. 5.0) (Rectangle.y p.body) (Rectangle.width p.body) (Rectangle.height p.body)
     else if is_key_down Key.D || is_key_down Key.Right then
-      Vector2.create ((Vector2.x p.pos) +. 5.0) (Vector2.y p.pos)
-    else p.pos
+      Rectangle.create ((Rectangle.x p.body) +. 5.0) (Rectangle.y p.body) (Rectangle.width p.body) (Rectangle.height p.body)
+    else p.body
   in
 
   let p_move_y px =
     if is_key_down Key.W || is_key_down Key.Up then
-      Vector2.create (Vector2.x px) ((Vector2.y px) -. 5.0)
+      Rectangle.create (Rectangle.x px) ((Rectangle.y px) -. 5.0) (Rectangle.width p.body) (Rectangle.height p.body)
     else if is_key_down Key.S || is_key_down Key.Down then
-      Vector2.create (Vector2.x px) ((Vector2.y px) +. 5.0)
+      Rectangle.create (Rectangle.x px) ((Rectangle.y px) +. 5.0) (Rectangle.width p.body) (Rectangle.height p.body)
     else px
   in
 
-  { pos = p_move_x |> p_move_y ; size = p.size; color = p.color }
+  { body = p_move_x |> p_move_y; color = p.color }
 
 let setup () =
   init_window 800 600 "game";
@@ -37,10 +41,11 @@ let setup () =
 let rec loop p () =
   if window_should_close () then close_window ()
   else begin
-  let p = p_move p in
+  let p = if check_collision_recs (p_move p).body e.body == false then p_move p else p in
   begin_drawing ();
   clear_background Color.skyblue; (*draw background*)
-  draw_rectangle_v p.pos p.size p.color;
+  draw_rectangle_rec e.body e.color; (*enemy player*)
+  draw_rectangle_rec p.body p.color; (*player player*)
   draw_text "Fuck yeah we doing an OCaml Game!" 400 300 20 Color.black;
   end_drawing ();
   loop p ()
